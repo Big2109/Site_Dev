820055617fc03b7669e036321ee29904b86f010b
Eric Bighi - Home <ericmbighi@gmail.com>
2026-05-28 12:41:08 -0300

    Updates

--- pages/Produtos.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import ProdutosSection from '../components/ProdutosSection.vue'
-</script>
-
-<template>
-  <ProdutosSection />
-</template>
--- types/Produto.ts
+++
@@ -1,7 +0,0 @@
-export interface Produto {
-    nome: string
-    preco: number
-    imagem: string
-    descricao?: string
-    vazio?: boolean
-}
